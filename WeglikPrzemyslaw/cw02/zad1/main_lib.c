4f0682502b93ccefb8fe4c51cf535507e4837e63
Przemek Weglik <przemek.weglik@onet.pl>
2022-03-25 13:26:53 +0100

    Fixed zad 1

--- WeglikPrzemyslaw/cw02/zad1/main_lib.c
+++ WeglikPrzemyslaw/cw02/zad1/main_lib.c
@@ -12,6 +12,46 @@
 int LINE_MAX_LENGTH = 10000;
 int MAX_FILENAME = 100;
 
+void process_arguments(int argc, char *argv[], char ** input_filename, char ** output_filename)
+{
+    if(argc == 3)
+    {
+        printf("Correct arguments! Copying file's content...\n");
+        *input_filename = calloc(sizeof(char), strlen(argv[1]));
+        strcpy(*input_filename, argv[1]);
+
+        *output_filename = calloc(sizeof(char), strlen(argv[2]));
+        strcpy(*output_filename, argv[2]);
+    }
+    else
+    {
+        *input_filename = calloc(sizeof(char), MAX_FILENAME);
+        *output_filename = calloc(sizeof(char), MAX_FILENAME);
+        printf("You haven't passed right arguments!\n");
+        printf("Input file: ");
+        scanf("%s", *input_filename);
+        printf("Output file: ");
+        scanf("%s", *output_filename);
+    }
+}
+
+void load_files(FILE ** input_file, FILE ** output_file, char ** input_filename, char ** output_filename)
+{
+    *input_file = fopen(*input_filename, "r");
+    if(*input_file == NULL)
+    {   
+        printf("Couldn't open input file: %s", *input_filename);
+        exit(1);
+    }
+
+    *output_file = fopen(*output_filename, "w");
+    if(*output_file == NULL)
+    {   
+        printf("Couldn't open output file: %s", *output_filename);
+        exit(1);
+    }
+}
+
 bool is_line_empty(char * line)
 {
     size_t i = 0;
@@ -24,6 +64,37 @@ bool is_line_empty(char * line)
         i++;
     }
     return true;
+}
+
+void filter_empty_lines(FILE ** input_file, FILE ** output_file)
+{
+    char * line = calloc(sizeof(char), LINE_MAX_LENGTH);
+    char character[1];
+    size_t len;
+    ssize_t read;
+
+    strcpy(line, "");
+    len = 0;
+    while((read = fread(character, sizeof(char), 1, *input_file)) == 1) {
+        if(strcmp(character, "\n") == 0)
+        {
+            if(is_line_empty(line) == false)
+            {
+                strcat(line, "\n");
+                len += 1;
+                fwrite(line, sizeof(char), len, *output_file);
+            }
+            // cleaning line and len
+            strcpy(line, "");
+            len = 0;
+        }
+        else
+        {
+            strcat(line, character);
+            len += 1;
+        }
+    }
+    free(line);
 }
 
 // functions used for timing
@@ -52,81 +123,24 @@ int main(int argc, char *argv[])
 {
     char * input_filename;
     char * output_filename;
-    if(argc == 3)
-    {
-        printf("Correct arguments! Copying file's content...\n");
-        input_filename = calloc(sizeof(char), strlen(argv[1]));
-        strcpy(input_filename, argv[1]);
-
-        output_filename = calloc(sizeof(char), strlen(argv[2]));
-        strcpy(output_filename, argv[2]);
-    }
-    else
-    {
-        input_filename = calloc(sizeof(char), MAX_FILENAME);
-        output_filename = calloc(sizeof(char), MAX_FILENAME);
-        printf("You haven't passed right arguments!\n");
-        printf("Input file: ");
-        scanf("%s", input_filename);
-        printf("Output file: ");
-        scanf("%s", output_filename);
-    }
+    
+    process_arguments(argc, argv, &input_filename, &output_filename);
 
     // start timing
     start_clock();
 
     FILE * input_file;
     FILE * output_file;
-    char * line = calloc(sizeof(char), LINE_MAX_LENGTH);
-    char character[1];
-    size_t len;
-    ssize_t read;
 
-    input_file = fopen(input_filename, "r");
-    if(input_file == NULL)
-    {   
-        printf("Couldn't open input file: %s", input_filename);
-        return 1;
-    }
+    load_files(&input_file, &output_file, &input_filename, &output_filename);
 
-    output_file = fopen(output_filename, "w");
-    if(output_file == NULL)
-    {   
-        printf("Couldn't open output file: %s", output_filename);
-        return 1;
-    }
-
-
-    strcpy(line, "");
-    len = 0;
-    while((read = fread(character, sizeof(char), 1, input_file)) == 1) {
-        if(strcmp(character, "\n") == 0)
-        {
-            if(is_line_empty(line) == false)
-            {
-                strcat(line, "\n");
-                len += 1;
-                fwrite(line, sizeof(char), len, output_file);
-            }
-            // cleaning line and len
-            strcpy(line, "");
-            len = 0;
-        }
-        else
-        {
-            strcat(line, character);
-            len += 1;
-        }
-
-
-    }
+    filter_empty_lines(&input_file, &output_file);
 
     end_clock();
 
     fclose(input_file);
     fclose(output_file);
 
-    free(line);
     free(input_filename);
     free(output_filename);
 
--- WeglikPrzemyslaw/cw02/zad1/main_sys.c
+++ WeglikPrzemyslaw/cw02/zad1/main_sys.c
@@ -12,6 +12,46 @@
 int LINE_MAX_LENGTH = 10000;
 int MAX_FILENAME = 100;
 
+void process_arguments(int argc, char *argv[], char ** input_filename, char ** output_filename)
+{
+    if(argc == 3)
+    {
+        printf("Correct arguments! Copying file's content...\n");
+        *input_filename = calloc(sizeof(char), strlen(argv[1]));
+        strcpy(*input_filename, argv[1]);
+
+        *output_filename = calloc(sizeof(char), strlen(argv[2]));
+        strcpy(*output_filename, argv[2]);
+    }
+    else
+    {
+        *input_filename = calloc(sizeof(char), MAX_FILENAME);
+        *output_filename = calloc(sizeof(char), MAX_FILENAME);
+        printf("You haven't passed right arguments!\n");
+        printf("Input file: ");
+        scanf("%s", *input_filename);
+        printf("Output file: ");
+        scanf("%s", *output_filename);
+    }
+}
+
+void load_files(int * input_file, int * output_file, char ** input_filename, char ** output_filename)
+{
+    *input_file = open(*input_filename, O_RDONLY);
+    if(*input_file == -1)
+    {   
+        printf("Couldn't open input file: %s", *input_filename);
+        exit(1);
+    }
+
+    *output_file = open(*output_filename, O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+    if(*output_file == -1)
+    {   
+        printf("Couldn't open output file: %s", *output_filename);
+        exit(1);
+    }
+}
+
 bool is_line_empty(char * line)
 {
     size_t i = 0;
@@ -24,6 +64,38 @@ bool is_line_empty(char * line)
         i++;
     }
     return true;
+}
+
+void filter_empty_lines(int * input_file, int * output_file)
+{
+    char * line = calloc(sizeof(char), LINE_MAX_LENGTH);
+    char character[1];
+    size_t len;
+
+    strcpy(line, "");
+    len = 0;
+
+    while(read(*input_file, character, 1) == 1) {
+        if(strcmp(character, "\n") == 0)
+        {
+            if(is_line_empty(line) == false)
+            {
+                strcat(line, "\n");
+                len += 1;
+                write(*output_file, line, len);
+            }
+            // cleaning line and len
+            strcpy(line, "");
+            len = 0;
+        }
+        else
+        {
+            strcat(line, character);
+            len += 1;
+        }
+    }
+
+    free(line);
 }
 
 // functions used for timing
@@ -52,80 +124,24 @@ int main(int argc, char *argv[])
 {
     char * input_filename;
     char * output_filename;
-    if(argc == 3)
-    {
-        printf("Correct arguments! Copying file's content...\n");
-        input_filename = calloc(sizeof(char), strlen(argv[1]));
-        strcpy(input_filename, argv[1]);
-
-        output_filename = calloc(sizeof(char), strlen(argv[2]));
-        strcpy(output_filename, argv[2]);
-    }
-    else
-    {
-        input_filename = calloc(sizeof(char), MAX_FILENAME);
-        output_filename = calloc(sizeof(char), MAX_FILENAME);
-        printf("You haven't passed right arguments!\n");
-        printf("Input file: ");
-        scanf("%s", input_filename);
-        printf("Output file: ");
-        scanf("%s", output_filename);
-    }
+    
+    process_arguments(argc, argv, &input_filename, &output_filename);
 
     // start timing
     start_clock();
 
     int input_file;
     int output_file;
-    char * line = calloc(sizeof(char), LINE_MAX_LENGTH);
-    char character[1];
-    size_t len;
 
-    input_file = open(input_filename, O_RDONLY);
-    if(input_file == -1)
-    {   
-        printf("Couldn't open input file: %s", input_filename);
-        return 1;
-    }
+    load_files(&input_file, &output_file, &input_filename, &output_filename);
 
-    output_file = open(output_filename, O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
-    if(output_file == -1)
-    {   
-        printf("Couldn't open output file: %s", output_filename);
-        return 1;
-    }
-
-
-    strcpy(line, "");
-    len = 0;
-    while(read(input_file, character, 1) == 1) {
-        if(strcmp(character, "\n") == 0)
-        {
-            if(is_line_empty(line) == false)
-            {
-                strcat(line, "\n");
-                len += 1;
-                write(output_file, line, len);
-            }
-            // cleaning line and len
-            strcpy(line, "");
-            len = 0;
-        }
-        else
-        {
-            strcat(line, character);
-            len += 1;
-        }
-
-
-    }
+    filter_empty_lines(&input_file, &output_file);
 
     end_clock();
 
     close(input_file);
     close(output_file);
 
-    free(line);
     free(input_filename);
     free(output_filename);
 
